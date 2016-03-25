36ba16a4095830307b1de12895f49296714f9b23
unknown <michalnebbia@gmail.com>
2016-03-25 21:49:58 +0100

    added replace for line breaks

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import TouchtypeApp from './TouchtypeApp.jsx';
 
 let text = `What suffering will have to be endured before 
-the workers realize that? It was from a man in Arizona. Test
- Test Test`
+the workers realize that? It was from a man in Arizona. What 
+suffering will have to be endured.`.replace(/(\n)+/g, '');
 
 export default class App extends React.Component {
   render() {
